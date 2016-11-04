a5bac02efdcceddfc9ee2835003c7770c120a876
Callie Bensel <callie.f.bensel@gmail.com>
2016-11-04 09:03:21 -0500

    saved css and js

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log( 'script.js sourced' );
+
+var buttonClick = function(){
+  alert( 'you clicked the button' )
+}; // end
+
+var showAlert = function(){
+  alert( 'something changed' )
+}; // end
